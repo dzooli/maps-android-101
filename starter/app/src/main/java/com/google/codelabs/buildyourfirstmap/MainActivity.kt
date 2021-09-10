d9d48ef06b3f7afdff419b06d44efd3c04002865
dzooli <zoltan.dzooli.fabian@gmail.com>
2021-09-10 16:22:58 +0200

    Step up to 8 - clustering and drawing

--- starter/app/src/main/java/com/google/codelabs/buildyourfirstmap/MainActivity.kt
+++ starter/app/src/main/java/com/google/codelabs/buildyourfirstmap/MainActivity.kt
@@ -20,15 +20,22 @@ import androidx.core.content.ContextCompat
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.Circle
+import com.google.android.gms.maps.model.CircleOptions
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.codelabs.buildyourfirstmap.place.Place
+import com.google.codelabs.buildyourfirstmap.place.PlaceRenderer
 import com.google.codelabs.buildyourfirstmap.place.PlacesReader
+import com.google.maps.android.clustering.ClusterItem
+import com.google.maps.android.clustering.ClusterManager
 
 class MainActivity : AppCompatActivity() {
 
     private val places: List<Place> by lazy {
         PlacesReader(this).read()
     }
+
+    private var circle:Circle? = null
 
     private val bicycleIcon: BitmapDescriptor by lazy {
         val color = ContextCompat.getColor(this, R.color.colorPrimary)
@@ -41,20 +48,46 @@ class MainActivity : AppCompatActivity() {
         val mapFragment = supportFragmentManager.findFragmentById(
             R.id.map_fragment
         ) as SupportMapFragment
-        mapFragment?.getMapAsync {
-            googleMap->addMarkers(googleMap)
-            googleMap.setInfoWindowAdapter(MarkerInfoWindowAdapter(this))
+        mapFragment.getMapAsync {
+            googleMap ->
+                //googleMap->addMarkers(googleMap)
+                //googleMap.setInfoWindowAdapter(MarkerInfoWindowAdapter(this))
+                addClusteredMarkers(googleMap)
         }
     }
 
-    private fun addMarkers(map: GoogleMap)
+    private fun addClusteredMarkers(googleMap: GoogleMap) {
+        val clusterManager = ClusterManager<Place>(this, googleMap)
+        clusterManager.renderer = PlaceRenderer(this, googleMap, clusterManager)
+        clusterManager.markerCollection.setInfoWindowAdapter(MarkerInfoWindowAdapter(this))
+        clusterManager.addItems(places)
+        clusterManager.cluster()
+        clusterManager.setOnClusterItemClickListener {
+            item -> addCircle(googleMap, item)
+            return@setOnClusterItemClickListener false
+        }
+        googleMap.setOnCameraIdleListener { clusterManager.onCameraIdle() }
+    }
+
+    private fun addCircle(googleMap: GoogleMap, item: Place)
     {
-        places.forEach{
-            place -> val marker = map.addMarker(
-            MarkerOptions()
-                .title(place.name)
-                .position(place.latLng)
-                .icon(bicycleIcon)
+        circle?.remove()
+        circle = googleMap.addCircle(
+            CircleOptions()
+                .center(item.latLng)
+                .radius(1000.0)
+                .fillColor(ContextCompat.getColor(this, R.color.colorPrimaryTranslucent))
+                .strokeColor(ContextCompat.getColor(this, R.color.colorPrimary))
+        )
+    }
+
+    private fun addMarkers(map: GoogleMap) {
+        places.forEach { place ->
+            val marker = map.addMarker(
+                MarkerOptions()
+                    .title(place.name)
+                    .position(place.latLng)
+                    .icon(bicycleIcon)
             )
             marker.tag = place
         }
